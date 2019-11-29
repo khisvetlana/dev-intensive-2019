bfbdc01bd139119c8e991dc96e1792c75689477c
khisvetlana <khisvetlana@yandex.ru>
2019-11-30 01:44:47 +0300

    DTO User, Factory **Паттерн Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -3,18 +3,16 @@ package ru.skillbranch.devintensive.models
 import ru.skillbranch.devintensive.ulits.Utils
 import java.util.*
 
-data class User(
-    val id: String,
-    var firstName: String?,
-    var lastName: String?,
-    var avatar: String?,
-    var rating: Int = 0,
-    var respect: Int = 0,
-    val lastVisit: Date? = null,
-    val isOnline: Boolean = false
+data class User private constructor(
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false
 ) {
-    var introBit : String
-
     constructor(id: String, firstName: String?, lastName: String?) : this(
         id = id,
         firstName = firstName,
@@ -22,43 +20,14 @@ data class User(
         avatar = null
     )
 
-    constructor(id: String) : this(id, "John", "Doe")
-
     init {
-        introBit = getIntro()
-
         println("It's Alive!!! \n" +
-                "${if (lastName === "Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName"} \n" /*+
-                "${getIntro()}"*/)
+                "${if (lastName === "Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName"} \n")
     }
-
-    private fun getIntro()="""
-        tu tu ru tuuuuu !!!
-        tu tu ru tuuuuuuuuu ...
-        
-        tu tu ru tuuuuu !!!
-        tu tu ru tuuuuuuuuu ...
-        
-        tu tu ru tuuuuu !!!
-        tu tu ru tuuuuuuuuu ...
-        ${"\n\n\n"}
-        $firstName $lastName
-        
-    """.trimIndent()
-
-    fun printMe() = println("""
-            id:  $id
-            firstName: $firstName
-            lastName: $lastName 
-            avatar: $avatar
-            rating: $rating
-            respect: $respect
-            lastVisit: $lastVisit
-            isOnline: $isOnline  
-        """.trimIndent())
 
     companion object Factory{
         private var lastId : Int = -1
+
         fun makeUser(fullName: String?) : User {
             lastId++
 
@@ -68,5 +37,25 @@ data class User(
         }
     }
 
-    fun sum(a: Int, b: Int) = a + b
+    class Builder(){
+        var id : String = ""
+        var firstName : String? = ""
+        var lastName : String? = ""
+        var avatar : String? = ""
+        var rating : Int = 0
+        var respect : Int = 0
+        var lastVisit : Date? = Date()
+        var isOnline : Boolean = false
+
+        fun id (id: String) = apply {this.id = id}
+        fun firstName (firstName: String) = apply {this.firstName = firstName}
+        fun lastName (lastName: String) = apply { this.lastName = lastName }
+        fun avatar (avatar: String) = apply {this.avatar = avatar}
+        fun rating (rating: Int) = apply { this.rating = rating }
+        fun respect (respect: Int) = apply { this.respect = respect }
+        fun lastVisit (lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun isOnline (isOnline: Boolean) = apply { {this.isOnline = isOnline} }
+
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline )
+    }
 }
