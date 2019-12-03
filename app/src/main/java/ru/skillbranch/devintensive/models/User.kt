af50ec24d9485540574b1b526db45f434c346fed
khisvetlana <khisvetlana@yandex.ru>
2019-12-03 13:15:47 +0300

    pattern builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -54,7 +54,7 @@ data class User constructor(
         fun rating (rating: Int) = apply { this.rating = rating }
         fun respect (respect: Int) = apply { this.respect = respect }
         fun lastVisit (lastVisit: Date) = apply { this.lastVisit = lastVisit }
-        fun isOnline (isOnline: Boolean) = apply { {this.isOnline = isOnline} }
+        fun isOnline (isOnline: Boolean) = apply { this.isOnline = isOnline }
 
         fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline )
     }
